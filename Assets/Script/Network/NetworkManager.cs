3650f6fe6c77356cd9337028b0c805daf71d23e0
Holger Schad <holgerschad@outlook.de>
2018-04-10 17:26:55 +0200

    first simple implementation of pun

---
+++ Assets/Script/Network/NetworkManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+    const string VERSION = "0.0.1";
+    public string roomName = "UnityTerraforming";
+
+    public string playerPrefabName = "Priest";
+    public Transform spawnPoint;
+
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings(VERSION);
+	}
+
+    void OnJoinedLobby()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsVisible = false;
+        roomOptions.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(playerPrefabName, spawnPoint.position, spawnPoint.rotation, 0);
+    }
+}
---
+++ Assets/Script/Network/NetworkPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityScript;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class NetworkPlayer : Photon.MonoBehaviour {
+
+    public GameObject myCamera;
+
+    // Use this for initialization
+    void Start () {
+        if(photonView.isMine)
+        {
+            myCamera.SetActive(true);
+            GetComponent<AudioSource>().enabled = true;
+            GetComponent<CharacterController>().enabled = true;
+            GetComponent<FirstPersonController>().enabled = true;
+        }
+        else
+        {
+            myCamera.SetActive(false);
+            GetComponent<AudioSource>().enabled = false;
+            GetComponent<CharacterController>().enabled = false;
+            GetComponent<FirstPersonController>().enabled = false;
+        }
+
+
+	}
+	
+}
