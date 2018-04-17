75277f9a4cda7f91ecbac903f4371921d392679c
Lars Assmus <lassmus@stud.hs-heilbronn.de>
2018-04-17 16:51:51 +0200

    sanftes anheben und absenken Implementiert

---
+++ Assets/Script/Terraforming/Brush/RoundBrush.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundBrush : Brush
+{
+    public override float[,] CalculateBrushUp(float[,] currentBrushValues)
+    {
+        float[,] ret = currentBrushValues;
+        int lengthY = currentBrushValues.GetLength(0);
+        int lengthX = currentBrushValues.GetLength(1);
+        int midX = lengthX / 2;
+        int midY = lengthY / 2;
+        int radius = BrushWidth / 2;
+
+        for (int y = 0; y < lengthY; y++)
+        {
+            for (int x = 0; x < lengthX; x++)
+            {
+                if (((x - midX) * (x - midX) + (y - midY) * (y - midY)) <= radius * radius)
+                {
+                    ret[y, x] += Value * sizeModificator;
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public override float[,] CalculateBrushDown(float[,] currentBrushValues)
+    {
+        float[,] ret = currentBrushValues;
+        int lengthY = currentBrushValues.GetLength(0);
+        int lengthX = currentBrushValues.GetLength(1);
+        int midX = lengthX / 2;
+        int midY = lengthY / 2;
+        int radius = BrushWidth / 2;
+        for (int y = 0; y < lengthY; y++)
+        {
+            for (int x = 0; x < lengthX; x++)
+            {
+                if (((x - midX) * (x - midX) + (y - midY) * (y - midY)) <= radius * radius)
+                {
+                    ret[y, x] -= Value * sizeModificator;
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    protected override void MakeIndicator()
+    {
+        if (HoverIndicator == null)
+        {
+            HoverIndicator = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            HoverIndicator.GetComponent<Renderer>().material = GoodIndicator;
+            HoverIndicator.SetActive(false);
+        }
+        HoverIndicator.transform.localScale = new Vector3(BrushWidth, .5f, BrushHeight);
+    }
+}
---
+++ Assets/Script/Terraforming/Brush/SmoothRoundBrush.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothRoundBrush : Brush
+{
+    public override float[,] CalculateBrushUp(float[,] currentBrushValues)
+    {
+        float[,] ret = currentBrushValues;
+        int lengthY = currentBrushValues.GetLength(0);
+        int lengthX = currentBrushValues.GetLength(1);
+        int midX = lengthX / 2;
+        int midY = lengthY / 2;
+        int radius = BrushWidth / 2;
+
+        for (int y = 0; y < lengthY; y++)
+        {
+            for (int x = 0; x < lengthX; x++)
+            {
+                float distance = ((x - midX) * (x - midX) + (y - midY) * (y - midY));
+                float quaterRadius2 = (radius * radius) / 4f;
+
+                if (distance <= quaterRadius2)
+                {
+                    ret[y, x] += Value;
+                }
+                else if (distance <= (radius * radius))
+                {
+                    float smoothing = (quaterRadius2 * 3f - (distance - quaterRadius2)) / (quaterRadius2 * 3f);
+                    ret[y, x] += Value * smoothing;
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public override float[,] CalculateBrushDown(float[,] currentBrushValues)
+    {
+        float[,] ret = currentBrushValues;
+        int lengthY = currentBrushValues.GetLength(0);
+        int lengthX = currentBrushValues.GetLength(1);
+        int midX = lengthX / 2;
+        int midY = lengthY / 2;
+        int radius = BrushWidth / 2;
+
+        for (int y = 0; y < lengthY; y++)
+        {
+            for (int x = 0; x < lengthX; x++)
+            {
+                float distance = ((x - midX) * (x - midX) + (y - midY) * (y - midY));
+                float quaterRadius2 = (radius * radius) / 4f;
+
+                if (distance <= quaterRadius2)
+                {
+                    ret[y, x] -= Value;
+                }
+                else if (distance <= (radius * radius))
+                {
+                    float smoothing = (quaterRadius2 * 3f - (distance - quaterRadius2)) / (quaterRadius2 * 3f);
+                    ret[y, x] -= Value * smoothing;
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    protected override void MakeIndicator()
+    {
+        if (HoverIndicator == null)
+        {
+            HoverIndicator = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            HoverIndicator.GetComponent<Renderer>().material = GoodIndicator;
+            HoverIndicator.SetActive(false);
+        }
+        HoverIndicator.transform.localScale = new Vector3(BrushWidth, .5f, BrushHeight);
+    }
+}
