0fba6ab630eae95fe7bb0d6520ea28141dd515e1
Dennis Gebhardt <dgebhard@stud.hs-heilbronn.de>
2018-04-20 09:51:10 +0200

    added wall for building

--- Assets/Script/BuildingScripts/BuildMode.cs
+++ Assets/Script/BuildingScripts/BuildMode.cs
@@ -3,22 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildMode : MonoBehaviour {
-
+    private enum buildMode
+    {
+            buildModeOff,
+            buildModeGround,
+            buildModeWall
+    }
     public Transform normalSquare;
     public Transform transparentSquare;
+    public Transform normalWall;
+    public Transform transparentWall;
     public Transform map;
     public float maxRay;
     public float gridSize;
 
     private Vector3 position;
     private bool isBuildModeActive;
+    private bool canBuild;
     private Transform square;
 
+
+
     protected vThirdPersonCamera tpCamera;
+    private buildMode presentBuildMode;
+
     // Use this for initialization
     void Start () {
         isBuildModeActive = false;
         tpCamera = FindObjectOfType<vThirdPersonCamera>();
+        presentBuildMode = buildMode.buildModeOff;
     }
 	
 	// Update is called once per frame
@@ -27,8 +40,22 @@ public class BuildMode : MonoBehaviour {
         {
             isBuildModeActive = true;
 
-
+            presentBuildMode = buildMode.buildModeGround;
+            if(square != null)
+            {
+                Destroy(square.gameObject);
+            }
             square = Instantiate(transparentSquare, position, Quaternion.identity);
+        }
+        if(Input.GetKeyUp(KeyCode.R))
+        {
+            isBuildModeActive = true;
+            presentBuildMode = buildMode.buildModeWall;
+            if (square != null)
+            {
+                Destroy(square.gameObject);
+            }
+            square = Instantiate(transparentWall, position, Quaternion.identity);
         }
         if (isBuildModeActive)
         {
@@ -51,11 +78,28 @@ public class BuildMode : MonoBehaviour {
 
                     position = finalPos;
                     square.position = finalPos;
-                }
+             }
             }
             if (Input.GetKeyUp(KeyCode.Mouse0))
             {
-                Instantiate(normalSquare, position, Quaternion.identity);
+                switch (presentBuildMode)
+                {
+                    case buildMode.buildModeGround:
+                        Instantiate(normalSquare, position, square.rotation);
+                        return;
+                    case buildMode.buildModeWall:
+                        Instantiate(normalWall, position, square.rotation);
+                        return;
+                    default:
+                        return;
+
+                }
+                
+                    
+
+                        
+                
+                
             }
         }      
 	}
@@ -64,16 +108,30 @@ public class BuildMode : MonoBehaviour {
     {
 
         float snapPointX = hitPoint.x + ((gridSize - hitPoint.x) % gridSize);
-        float snapPointY = hitPoint.y + 0.05f;
+        float snapPointY = hitPoint.y + ((gridSize - hitPoint.y) % gridSize);
         float snapPointZ = hitPoint.z + ((gridSize - hitPoint.z) % gridSize);
         float halfGridSize = gridSize / 2;
 
         if (!(snapPointX + halfGridSize >= hitPoint.x)) snapPointX += gridSize;
         else if (!(snapPointX - halfGridSize <= hitPoint.x)) snapPointX -= gridSize;
 
+        if (!(snapPointY + halfGridSize >= hitPoint.y)) snapPointY += gridSize;
+        else if (!(snapPointY - halfGridSize <= hitPoint.y)) snapPointY -= gridSize;
+
         if (!(snapPointZ + halfGridSize >= hitPoint.z)) snapPointZ += gridSize;
         else if (!(snapPointZ - halfGridSize <= hitPoint.z)) snapPointZ -= gridSize;
 
+        var rotation = tpCamera.transform.rotation.eulerAngles;
+        rotation.x = 0;
+        rotation.y = Mathf.Round(rotation.y / 90) * 90;
+        rotation.z = 0;
+        square.rotation = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
+        if (presentBuildMode == buildMode.buildModeWall)
+        {
+            snapPointX += halfGridSize;
+            snapPointY += halfGridSize;
+            //snapPointZ += halfGridSize;
+        }
         Debug.Log("hitpoint x:" + hitPoint.x + " hitpoint z: " + hitPoint.z + " snappoint x:" + snapPointX + " snappoint z:" + snapPointZ + " mod x:" + (gridSize - hitPoint.x) % gridSize);
         Vector3 gridPosition = new Vector3(snapPointX, snapPointY, snapPointZ);
         Debug.Log("grid position:" + gridPosition);
