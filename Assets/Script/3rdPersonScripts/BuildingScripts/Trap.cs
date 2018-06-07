1e78c2a5918a8c989bddcbe346a9df5b860a9d6b
Holger Schad <holgerschad@outlook.de>
2018-06-07 10:07:44 +0200

    added 2 traps

---
+++ Assets/Script/3rdPersonScripts/BuildingScripts/Trap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+
+    public float damagePerTick;
+    public int ticks;
+    public int duration;
+
+    private float tickTimer;
+    private float nextTickTime;
+    private bool activated;
+    private float timeLeft;
+
+    private void Start()
+    {
+        activated = false;
+        nextTickTime = duration / ticks;
+
+        if (ticks == 1 && duration > 1)
+            duration = 1;
+    }
+
+    private void Update()
+    {
+        if (activated && ticks > 0)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!activated)
+        {
+            activated = true;
+        }
+    }
+
+}
