ff0b9e6d258c20ba4ed7b37a6b91a128d6d7ea14
StyZZy <dgebhard@stud.hs-heilbronn.de>
2018-06-12 15:45:23 +0200

    3rd person networkable

--- Assets/WorldUIManager.cs
+++ Assets/WorldUIManager.cs
@@ -10,24 +10,24 @@ public class WorldUIManager : MonoBehaviour {
     private vThirdPersonCamera camera;
 	// Use this for initialization
 	void Start () {
-        player = GameObject.FindGameObjectWithTag("Player");
-        camera = FindObjectOfType<vThirdPersonCamera>();
+        GameObject.FindGameObjectWithTag("Player");
+
         pickupCanvas.SetActive(false);       
     }
 	
 	// Update is called once per frame
 	void Update () {
-        pickupCanvas.transform.rotation = camera.transform.rotation;
+        pickupCanvas.transform.rotation = Camera.main.transform.rotation;
         if (pickupCanvas.active == true)
         {
            if (Input.GetKeyUp(KeyCode.E))
             {
                 if(gameObject.GetComponent<ResourceObject>().inventoryIcon != null)
                 {
-                    player.GetComponent<ResourceControl>().PickupItem(gameObject);
+                    GameObject.FindGameObjectWithTag("Player").GetComponent<ResourceControl>().PickupItem(gameObject);
                 }
                 ResourceObject resourceTaken = gameObject.GetComponent<ResourceObject>();
-                player.GetComponent<ResourceControl>().AddResource(resourceTaken.resourceType, resourceTaken.resourceAmount);
+                GameObject.FindGameObjectWithTag("Player").GetComponent<ResourceControl>().AddResource(resourceTaken.resourceType, resourceTaken.resourceAmount);
                 pickupCanvas.SetActive(false);
                 Destroy(gameObject);
             }           
